3f6ec17900e0b57905d45b4890872f4aaaf73cc2
Benjamin H <bensku2008@gmail.com>
2025-01-16 10:21:21 +0200

    fixed ui.h file not included

---
+++ include/Sandboxia/ui.h
@@ -0,0 +1,23 @@
+#ifndef UI_H
+#define UI_H
+
+#include "../main.h"
+#include "player.h"
+#include "renderer.h"
+
+#include "../imgui/imgui.h"
+#include "../imgui/imgui_impl_glfw.h"
+#include "../imgui/imgui_impl_opengl3.h"
+
+extern bool isConsoleOpen;
+extern bool isMenuOpen;
+
+void InitUI(GLFWwindow* window);
+void HandleUI(GLFWwindow* window, World &world, Player &player, Renderer &renderer);
+void ConsoleUI();
+void DebugUI();
+void MenuUI(GLFWwindow* window ,World &world);
+void ExitUI();
+
+
+#endif
